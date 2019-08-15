0f0ebf9ff578840512d14f479698af38d39d30ff
Nikola Pocuca <nikpocuca@gmail.com>
2019-08-15 09:30:43 -0400

    added pass by reference values

--- src/julia/ccall_examples/calc_mean.c
+++ src/julia/ccall_examples/calc_mean.c
@@ -1,7 +1,19 @@
+
+
+// Pass by value examples
 double sum_mean(double a, double b) {
   return (a+b) ;
 }
 
+
 double something(double a, double b){
 	return(a + b + 1); 
 }
+
+// Pass by Reference
+void passRef(int* i){
+ i[0] += 1; 
+};
+
+
+
